544058b9bc67e9f3849ace97e029738485a9b708
beta-7 <david.milanovski@students.finki.ukim.mk>
2022-06-13 23:50:08 +0200

    second commit (forgot files lol)

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            {{ __('A fresh verification link has been sent to your email address.') }}
+                        </div>
+                    @endif
+
+                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('If you did not receive the email') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+
+                        @if (Auth::user())
+                            <div class="alert alert-success" role="alert">
+                                Welcome {{Auth::user()->name}}
+                            </div>
+                        @endif
+
+                            <p>
+                                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                    Upload files!
+                                </a>
+
+                            </p>
+
+
+                            <div class="collapse " id="collapseExample">
+                                <div class="card card-body btn-group-lg">
+
+                                    <form class="mb-3" action="/upload" method="post">
+                                        @csrf
+                                        <label for="uploadName"  class="form-label">Name the upload group (will be public)</label>
+                                        <textarea class="form-control" id="uploadName" name="uploadName" rows="1"></textarea>
+
+                                        <label for="days" class="form-label">When should the files delete</label>
+                                        <select class="form-select" id="days" name="delete" aria-label="Delete the file(s) after">
+                                            <option value="never">Never</option>
+                                            <option value="1">1 day</option>
+                                            <option value="2">2 days</option>
+                                            <option value="3">3 days</option>
+                                            <option value="5">5 days</option>
+                                            <option value="10">10 days</option>
+                                            <option value="14">14 days</option>
+                                            <option value="delete">After first download</option>
+                                        </select>
+                                        <br>
+
+
+
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" name="encrypt" id="encrypt">
+                                        <label class="form-check-label" for="encrypt">
+                                            Encrypt the files
+                                        </label>
+                                        </div>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="sendMethod" data-parent="#collapseExample" data-toggle="collapse" data-target="#emailInput" value="link" id="radioLink" checked="checked">
+                                            <label class="form-check-label" for="radioLink">
+                                                Send with a link
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="sendMethod" data-parent="#collapseExample" data-toggle="collapse" data-target="#emailInput" value="email" id="radioEmail">
+                                            <label class="form-check-label" for="radioEmail">
+                                                Send with email
+                                            </label>
+                                        </div>
+
+                                        <div id="emailInput" class="collapse">
+                                            <label for="exampleInputEmail1">Email address</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Enter email">
+                                        </div>
+                                        <label for="files" class="form-label">Select file(s) for upload</label>
+                                        <input class="form-control" type="file" name="files" id="files" multiple>
+
+                                        <button type="submit" class="btn btn-success btn-block">Submit</button>
+                                    </form>
+
+                                    </div>
+
+
+
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
